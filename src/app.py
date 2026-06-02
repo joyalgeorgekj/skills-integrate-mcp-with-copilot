2a67f06feba516cf7cba41beae0c7969d8c44dc4
Joyal George K J <144530321+joyalgeorgekj@users.noreply.github.com>
2026-06-02 22:55:10 +0000

    Solved 1 issue

--- src/app.py
+++ src/app.py
@@ -27,6 +27,12 @@ activities = {
         "max_participants": 12,
         "participants": ["michael@mergington.edu", "daniel@mergington.edu"]
     },
+    "GitHub Skills": {
+        "description": "Join the GitHub Skills program to learn practical coding and collaboration tools.",
+        "schedule": "Mondays and Thursdays, 4:00 PM - 5:00 PM",
+        "max_participants": 25,
+        "participants": []
+    },
     "Programming Class": {
         "description": "Learn programming fundamentals and build software projects",
         "schedule": "Tuesdays and Thursdays, 3:30 PM - 4:30 PM",
